0bbbe6be6a59fc91d3d02c235d5cfa943f689efd
Nicolás Ardila <johan.ardilah@gmail.com>
2023-04-25 22:47:47 -0500

    recursividad

---
+++ Recursividad/Clase_Principal.java
@@ -0,0 +1,8 @@
+package Recursividad;
+
+public class Clase_Principal {
+    public static void main(String[] args) {
+        Recursividad recursividad = new Recursividad();
+        recursividad.Imprimir(1);
+    }
+}
